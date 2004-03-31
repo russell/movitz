947309eb7e2cd8adc6d45ac530f8515bb210d095
Frode V. Fjeld <frodevf@gmail.com>
2004-03-31 16:34:47 +0000

    Be slightly more clever about guessing a nil-word for stream-images.

--- stream-image.lisp
+++ stream-image.lisp
@@ -31,7 +31,11 @@
     :reader image-start-address)
    (nil-word
     :initarg :nil-word
-    :initform #x65
+    :initform (if (boundp '*image*)
+		  (image-nil-word *image*)
+		(progn
+		  (format *query-io* "~&Please enter the stream-images NIL value: ")
+		  (read *query-io*)))
     :reader image-nil-word)))
 
 (defmethod image-register32 ((image stream-image) register-name)
