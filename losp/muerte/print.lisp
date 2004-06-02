e1f9c724621f27f5d4e7154a8c28bbc6b0a98c5b
Frode V. Fjeld <frodevf@gmail.com>
2004-06-02 23:50:34 +0000

    Removed hack from write-integer because there are bignums now.

--- losp/muerte/print.lisp
+++ losp/muerte/print.lisp
@@ -113,25 +113,15 @@
       (t (write-char #\# stream)
 	 (write-simple-integer base 10 stream)
 	 (write-char #\r stream))))
-  (block minus-hack			; don't barf on most-negative-fixnum
-    (multiple-value-bind (sign-char print-value)
-	(cond
-	 ((minusp x)
-	  (if (not (eq x most-negative-fixnum))
-	      (values #\- (- x))
-	    (return-from minus-hack
-	      (write-string (case base
-			      (2 #.(cl:format cl:nil "~B" movitz::+movitz-most-negative-fixnum+))
-			      (8 #.(cl:format cl:nil "~O" movitz::+movitz-most-negative-fixnum+))
-			      (10 #.(cl:format cl:nil "~D" movitz::+movitz-most-negative-fixnum+))
-			      (16 #.(cl:format cl:nil "~X" movitz::+movitz-most-negative-fixnum+))
-			      (t (break "minus-hack!?")))
-			    stream))))
-	 (sign-always
-	  (values #\+ x))
-	 (t (values nil x)))
-      (write-lowlevel-integer print-value stream base comma-char comma-interval
-			      mincol padchar sign-char 0)))
+  (multiple-value-bind (sign-char print-value)
+      (cond
+       ((minusp x)
+	(values #\- (- x)))
+       (sign-always
+	(values #\+ x))
+       (t (values nil x)))
+    (write-lowlevel-integer print-value stream base comma-char comma-interval
+			    mincol padchar sign-char 0))
   (when (and radix (= 10 base))
     (write-char #\. stream))
   nil)
