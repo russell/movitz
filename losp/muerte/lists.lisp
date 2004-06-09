aa5c1eef1e678cee48ba41f72a3e4383ccee31dd
Frode V. Fjeld <frodevf@gmail.com>
2004-06-09 20:18:45 +0000

    Added adjoin.

--- losp/muerte/lists.lisp
+++ losp/muerte/lists.lisp
@@ -436,3 +436,18 @@
 
   
   
+(defun subsetp (list-1 list-2 &key (key 'identity) (test 'eql) test-not)
+  "=> generalized-boolean"
+  (let ((test (if test-not
+		  (complement test-not)
+		test)))
+    (dolist (x list-1 t)
+      (unless (member x list-2 :key key :test test)
+	(return nil)))))
+
+(defun adjoin (item list &key (key 'identity) (test 'eql) test-not)
+  "=> new-list"
+  (let ((test (if test-not (complement test-not) test)))
+    (if (member (funcall key item) list :test test)
+	list
+      (cons item list))))
