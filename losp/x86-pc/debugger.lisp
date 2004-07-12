a08a6595c8ae9baec5a2aa75c8da678c4a85ed7a
Frode V. Fjeld <frodevf@gmail.com>
2004-07-12 11:10:40 +0000

    Slight fix to *call-site-patterns*, so that the arglist to recursive calls might be recognized.

--- losp/x86-pc/debugger.lisp
+++ losp/x86-pc/debugger.lisp
@@ -220,10 +220,10 @@ It is quite possible to return success without having found the result-{register
 			    (#x8b #x5e (:ebx :esi)) ; (:movl (:esi x) :ebx)
 			    (#x33 #xdb (:constant :ebx 0))
 			    )))
-		(:or ((:or (#x8b #x56 (:edx :esi)) ;     (:movl (:esi x) :edx)
-			   (#x8b #x54 #x37 (:edx :esi+edi))) ;#<asm MOVL [#x39+%EDI+%ESI] => %EDX>
-		      #x8b #x72 #xfd)	;     (:movl (:edx -3) :esi)
-		     (#x8b #x74 #x7e (:any-offset))) ; #<asm MOVL [#x28+%ESI+%EDI*2] => %ESI>
+		(:* 1 ((:or ((:or (#x8b #x56 (:edx :esi)) ;     (:movl (:esi x) :edx)
+				  (#x8b #x54 #x37 (:edx :esi+edi))) ;#<asm MOVL [#x39+%EDI+%ESI] => %EDX>
+			     #x8b #x72 #xfd) ;     (:movl (:edx -3) :esi)
+			    (#x8b #x74 #x7e (:any-offset))))) ; #<asm MOVL [#x28+%ESI+%EDI*2] => %ESI>
 		(:* 1 ((:or (#xb1 (:cl-numargs))))) ; (:movb x :cl)
 		(:* 1 ((:or (#x8b #x55 (:edx :ebp))
 			    (#x8b #x56 (:edx :esi)))))
