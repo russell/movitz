7a55d96544e24470ddfbaabecc2ca09ca5df1e2a
Frode V. Fjeld <frodevf@gmail.com>
2004-07-23 15:36:46 +0000

    Improved copy-control-stack: Take a parameter absolutep which means to make the stack-frame uplink pointers array indexes rather than locations.

--- losp/muerte/inspect.lisp
+++ losp/muerte/inspect.lisp
@@ -340,8 +340,9 @@ Obviously, this correspondence is not guaranteed to hold e.g. across GC."
 	      (* 2 (truncate (+ (structure-object-length object) 1) 2))))))))
 
 
-(defun copy-control-stack (&optional (stack (%run-time-context-slot 'stack-vector))
-				     (frame (current-stack-frame)))
+(defun copy-control-stack (&key (absolutep)
+				(stack (%run-time-context-slot 'stack-vector))
+				(frame (current-stack-frame)))
   (assert (location-in-object-p stack frame))
   (let* ((stack-start-location (+ 2 (object-location stack)))
 	 (frame-index (- frame stack-start-location))
@@ -357,7 +358,10 @@ Obviously, this correspondence is not guaranteed to hold e.g. across GC."
 	 (t (let ((uplink-index (- uplink-frame stack-start-location frame-index)))
 	      (assert (< -1 uplink-index (length copy)) ()
 		"Uplink-index outside copy: ~S, i: ~S" uplink-index i)
-	      (let ((x  (+ uplink-index copy-start-location)))
-		(assert (location-in-object-p copy x))
-		(setf (svref%unsafe copy i) x)
-		(setf i uplink-index)))))))))
+	      (setf (svref%unsafe copy i)
+		(if absolutep
+		    uplink-index
+		  (let ((x (+ uplink-index copy-start-location)))
+		    (assert (location-in-object-p copy x))
+		    (setf (svref%unsafe copy i) x))))
+	      (setf i uplink-index))))))))
