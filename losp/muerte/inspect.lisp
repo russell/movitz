e9f99d25227de353ecde9c08b5ae07401a9ae254
Frode V. Fjeld <frodevf@gmail.com>
2004-07-29 12:51:40 +0000

    Fixed objects-equalp for symbols.

--- losp/muerte/inspect.lisp
+++ losp/muerte/inspect.lisp
@@ -229,12 +229,12 @@ after the point that called this stack-frame."
 		  (dotimes (i (funobj-num-constants x) t)
 		    (unless (test funobj-constant-ref i)))))
 	    (symbol
-	     (and ;; (test memref -7 0 :lisp) ; value
-		  (test memref -7 1 :lisp)    ; function-value
-		  ;; (test memref -7 2 :lisp) ; plist
-		  (test memref -7 3 :lisp)    ; name
-		  ;; (test memref -7 4 :lisp) ; package
-		  (test memref -7 5 :lisp))) ; flags
+	     (and (test memref #.(bt:slot-offset 'movitz:movitz-symbol 'movitz::function-value)
+			0 :lisp)
+		  (test memref #.(bt:slot-offset 'movitz:movitz-symbol 'movitz::name)
+			0 :lisp)
+		  (test memref #.(bt:slot-offset 'movitz:movitz-symbol 'movitz::flags)
+			0 :lisp)))
 	    (vector
 	     (and (typep y 'vector)
 		  (test array-element-type)
