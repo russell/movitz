7e48c944d2d59a37d5017987bc5237a7ae83534d
Frode V. Fjeld <frodevf@gmail.com>
2004-08-23 13:58:07 +0000

    Changed the way stack locations are represented: Rather than merely a 'location' (which is a simple pointer, and so GC-unsafe), we now use two values: a vector and an index. If vector is non-nil, index is a an index into the vector. If vector is nil, index is a location (as before), typically referencing the currently active stack, which won't move (but probably this mode should be deprecated).

--- losp/muerte/conditions.lisp
+++ losp/muerte/conditions.lisp
@@ -285,7 +285,7 @@ Return the condition object, if there was one."
 			(format t "~%Condition for debugger: ~Z" condition)
 			(format t "~%No abort restart is active. Halting CPU.")
 			(halt-cpu))))
-   (t (let ((*debugger-invoked-stack-frame* (stack-frame-uplink (current-stack-frame))))
+   (t (let ((*debugger-invoked-stack-frame* (stack-frame-uplink nil (current-stack-frame))))
 	(funcall *debugger-function* condition))))
   (format *debug-io* "~&Debugger ~@[on ~S ]returned!~%Trying to abort...~%" condition)
   (let ((r (find-restart 'abort)))
