8706aeca3b8e3db8c7e71342f5ee56a28666ca60
Frode V. Fjeld <frodevf@gmail.com>
2004-11-23 16:09:34 +0000

    New dynamic variable protocol.

--- losp/muerte/symbols.lisp
+++ losp/muerte/symbols.lisp
@@ -44,16 +44,18 @@
     (null nil)
     (symbol
      (with-inline-assembly (:returns :eax)
-       (:compile-form (:result-mode :eax) symbol)
+       (:compile-form (:result-mode :ebx) symbol)
        (:call-local-pf dynamic-variable-lookup)
-       (:locally (:cmpl :eax (:edi (:edi-offset unbound-value))))
-       (:je '(:sub-program (unbound) (:int 99)))))))
+       (:cmpl -1 :eax)
+       (:into)))))
+;;;       (:locally (:cmpl :eax (:edi (:edi-offset unbound-value))))
+;;;       (:je '(:sub-program (unbound) (:int 99)))))))
 
 (defun %unbounded-symbol-value (symbol)
   "Return the symbol's value without checking if it's bound or not."
   (check-type symbol symbol)
   (with-inline-assembly (:returns :eax)
-    (:compile-form (:result-mode :eax) symbol)
+    (:compile-form (:result-mode :ebx) symbol)
     (:call-local-pf dynamic-variable-lookup)
    done))
 
@@ -129,7 +131,7 @@
 
 (defun makunbound (symbol)
   (setf (symbol-value symbol)
-    (load-global-constant unbound-value))
+    (load-global-constant new-unbound-value))
   symbol)
 
 (defun fboundp (symbol)
@@ -138,7 +140,7 @@
 
 (defun %create-symbol (name &optional (package nil)
 				      (plist nil)
-				      (value (load-global-constant unbound-value))
+				      (value (load-global-constant new-unbound-value))
 				      (function (load-global-constant movitz::unbound-function))
 				      (flags 0))
   (eval-when (:compile-toplevel)
