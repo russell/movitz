0f3f1f6fa6ccb534bd492efa7fac13f3cf5f049d
Frode V. Fjeld <frodevf@gmail.com>
2004-12-10 12:47:22 +0000

    *** empty log message ***

--- losp/lib/named-integers.lisp
+++ losp/lib/named-integers.lisp
@@ -85,7 +85,7 @@
 			     ',integer-list
 			     ',name-list))))
 	     (eval-when (:compile-toplevel)
-	       ,@constant-declarations
+;;;	       ,@constant-declarations
 	       (export ',constant-exports))
 	     ,@constant-declarations
 	     ',type-name))))
