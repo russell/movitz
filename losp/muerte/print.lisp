fd9f8ee1ef92b1f82c76f7b9103f4085c0a6ce58
Frode V. Fjeld <frodevf@gmail.com>
2005-01-17 11:02:27 +0000

    *** empty log message ***

--- losp/muerte/print.lisp
+++ losp/muerte/print.lisp
@@ -253,13 +253,15 @@
 		      (let ((name (symbol-name symbol)))
 			(if (and (plusp (length name))
 				 (every (lambda (c)
-					  (and (or (upper-case-p c)
-						   (member c '(#\+ #\- #\% #\$ #\* #\@ #\. #\&
-							       #\/ #\< #\> #\=))
-						   (digit-char-p c))
-					       (not (or (digit-char-p c *read-base*)
-							(member c '(#\.))))))
-					name))
+					  (or (upper-case-p c)
+					      (member c '(#\+ #\- #\% #\$ #\* #\@ #\. #\&
+							  #\/ #\< #\> #\=))
+					      (digit-char-p c)))
+					name)
+				 (not (every (lambda (c)
+					       (or (digit-char-p c *read-base*)
+						   (member c '(#\.))))
+					     name)))
 			    (write-string name stream)
 			  (stream-write-escaped-string stream name #\|)))))
 	       (cond
