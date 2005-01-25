840085537dce96eb73f6dc4847bbf68de1cb4d93
Frode V. Fjeld <frodevf@gmail.com>
2005-01-25 13:46:54 +0000

    Renamed funobj-frame-raw-locals. Added funobj-frame-headers-p.

--- losp/muerte/functions.lisp
+++ losp/muerte/functions.lisp
@@ -1,6 +1,6 @@
 ;;;;------------------------------------------------------------------
 ;;;; 
-;;;;    Copyright (C) 2001-2004, 
+;;;;    Copyright (C) 2001-2005, 
 ;;;;    Department of Computer Science, University of Tromso, Norway.
 ;;;; 
 ;;;;    For distribution policy, see the accompanying file COPYING.
@@ -388,10 +388,15 @@ represented as that vector."
   (check-type funobj function)
   (memref funobj (movitz-type-slot-offset 'movitz-funobj 'debug-info) :type :unsigned-byte16))
 
-(defun funobj-frame-num-unboxed (funobj)
+(defun funobj-frame-raw-locals (funobj)
   "The number of unboxed slots in this function's stack-frame(s)."
   (declare (ignore funobj))
   0)
+
+(defun funobj-frame-headers-p (funobj)
+  "Can this function place header-vals in its stack-frame?"
+  (declare (ignore funobj))
+  t)
 
 (defun make-funobj (&key (name :unnamed)
 			 (code-vector (funobj-code-vector #'constantly-prototype))
