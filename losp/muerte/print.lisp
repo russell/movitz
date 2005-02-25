011a5270460e40ec0afb1c9703605f25ff6f510d
Frode V. Fjeld <frodevf@gmail.com>
2005-02-25 08:00:11 +0000

    Minor change to the way errors are printed under *print-safely*.

--- losp/muerte/print.lisp
+++ losp/muerte/print.lisp
@@ -194,7 +194,7 @@
 	(handler-case (internal-write object)
 	  (serious-condition (c)
 	    (print-unreadable-object (c *standard-output* :type t :identity t)
-	      (format t " while printing ~Z" object))))))))
+	      (format t " (while printing ~Z)" object))))))))
 
 (defun internal-write (object)
   (let ((stream *standard-output*))
