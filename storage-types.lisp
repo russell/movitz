8fa451ec6d12626460a2e1661589a08eb2765de3
Frode V. Fjeld <frodevf@gmail.com>
2005-04-30 21:15:35 +0000

    Cleaned up the unbound-value protocol a bit.

--- storage-types.lisp
+++ storage-types.lisp
@@ -151,11 +151,9 @@
   (ecase type
     (word
      (cond
-       ((eq expr 'unbound)
-	(slot-value (image-run-time-context *image*) 'new-unbound-value))
-       ((typep expr 'movitz-object)
-	(movitz-intern expr))
-       (t (movitz-intern (movitz-read expr)))))
+      ((typep expr 'movitz-object)
+       (movitz-intern expr))
+      (t (movitz-intern (movitz-read expr)))))
     (code-vector-word
      (movitz-intern-code-vector expr))))
 
@@ -533,6 +531,14 @@ integer (native lisp) value."
 (deftype movitz-string ()
   '(satisfies movitz-stringp))
 
+;;;
+
+(define-binary-class movitz-unbound-value (movitz-immediate-object)
+  ())
+
+(defmethod movitz-intern ((obj movitz-unbound-value) &optional type)
+  (declare (ignore type))
+  #x7fffffff)
 
 ;;; Symbols
 
