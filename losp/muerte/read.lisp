b051b5907f337750cf84807b0273a5dd15b2ea7b
Frode V. Fjeld <frodevf@gmail.com>
2005-06-10 18:35:01 +0000

    Added a silly read-from-string.

--- losp/muerte/read.lisp
+++ losp/muerte/read.lisp
@@ -1,6 +1,6 @@
 ;;;;------------------------------------------------------------------
 ;;;; 
-;;;;    Copyright (C) 2001-2004, 
+;;;;    Copyright (C) 2001-2005, 
 ;;;;    Department of Computer Science, University of Tromso, Norway.
 ;;;; 
 ;;;;    For distribution policy, see the accompanying file COPYING.
@@ -342,6 +342,10 @@ of string delimited by start and end."
 				  (substring string i token-end))))))))))
       (t (return-from simple-read-from-string
 	   (simple-read-token string :start i :end end))))))
+
+(defun read-from-string (&rest args)
+  (declare (dynamic-extent args))
+  (apply #'simple-read-from-string args))
 
 (defun un-backquote (form level)
   "Dont ask.."
