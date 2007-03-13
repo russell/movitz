23a794ef74dc96bb8e6b093cbb1663b6962f92d7
Frode V. Fjeld <frodevf@gmail.com>
2007-03-13 20:42:42 +0000

    Remove debug statement.

--- losp/muerte/los-closette-compiler.lisp
+++ losp/muerte/los-closette-compiler.lisp
@@ -693,10 +693,6 @@
               (slot-initform (muerte::translate-program (slot-definition-initform slot)
                                                         '#:muerte.cl '#:cl)))
           (when slot-initform
-            (warn "init slot: ~S: ~S => ~S"
-                  slot-name
-                  slot-initform
-                  (movitz::eval-form slot-initform))
             (setf (movitz-slot-value class slot-name) (movitz::eval-form slot-initform)))))
       (std-after-initialization-for-classes class
 					    :direct-slots direct-slots
