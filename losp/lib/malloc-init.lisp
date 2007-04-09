556cbf1155f57b118eb010be0e1c284a8569408f
Frode V. Fjeld <frodevf@gmail.com>
2007-04-09 16:01:53 +0000

    Compute available memory correctly in initial allocator.

--- losp/lib/malloc-init.lisp
+++ losp/lib/malloc-init.lisp
@@ -28,7 +28,7 @@
        (start-location (logand (+ kernel-end-location (1- 4096/4)) -4096/4))
        ;; End-location is the end of the memory.
        (end-location (* (1- memsize-mb) 1024 1024/4)))
-  (muerte:malloc-initialize start-location end-location)
+  (muerte:malloc-initialize start-location (- end-location start-location))
   (setf (cdar muerte::%memory-map%) end-location)
   (loop for x from kernel-end-location below start-location
       do (setf (memref x 0 :type :unsigned-byte32) 0))
